e8edd3b6aeb9fe97e55f96c6ebde7b8dd83bc288
San4255 <sansaxena02@gmail.com>
2022-11-29 18:34:09 +0530

    superior bringup

--- superior_phoenix.mk
+++ superior_phoenix.mk
@@ -7,10 +7,10 @@
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_phoenix
+PRODUCT_NAME := superior_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X2
@@ -22,12 +22,10 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_ENABLE_BLUR := false
-WITH_GMS := true
+SUPERIOR_OFFICIAL := false
+SUPERIOR_GAPPS := minimal
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-RICE_MAINTAINER := Risan
-RICE_PACKAGE_TYPE := GAPPS
-TARGET_OPTOUT_GOOGLE_TELEPHONY := true
-TARGET_BUILD_GRAPHENEOS_CAMERA := true
 
 # Fingerprint
      PRODUCT_BUILD_PROP_OVERRIDES += \
